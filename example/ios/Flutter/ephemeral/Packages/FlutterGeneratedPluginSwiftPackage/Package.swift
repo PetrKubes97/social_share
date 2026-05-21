16d81d1eae9ec35c2dd49b5dab83a5d6fc024567
Petr Kubes <petrkubes1997@gmail.com>
2026-05-21 13:06:44 +0200

    android migration

---
+++ example/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// Generated file. Do not edit.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "FlutterGeneratedPluginSwiftPackage",
+    platforms: [
+        .iOS("13.0")
+    ],
+    products: [
+        .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
+    ],
+    dependencies: [
+        .package(name: "path_provider_foundation", path: "../.packages/path_provider_foundation-2.4.0"),
+        .package(name: "image_picker_ios", path: "../.packages/image_picker_ios-0.8.12"),
+        .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
+    ],
+    targets: [
+        .target(
+            name: "FlutterGeneratedPluginSwiftPackage",
+            dependencies: [
+                .product(name: "path-provider-foundation", package: "path_provider_foundation"),
+                .product(name: "image-picker-ios", package: "image_picker_ios"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ]
+        )
+    ]
+)
